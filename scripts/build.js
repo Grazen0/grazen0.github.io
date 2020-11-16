eb17e21fa59b4c0512f5a889196e99f9c6638c45
ElCholoGamer <josedanielgrayson@gmail.com>
2020-11-16 12:49:31 -0500

    poggers

---
+++ scripts/build.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
+
+console.log('Bundling project...');
+
+function copyFolder(folder, out) {
+	// Create folder if not exists
+	if (!fs.existsSync(out)) fs.mkdirSync(out);
+	const files = fs.readdirSync(folder);
+
+	// Recursion :D
+	files.forEach(file => {
+		const filePath = path.join(folder, file);
+		const outPath = path.join(out, file);
+
+		if (fs.statSync(filePath).isDirectory()) copyFolder(filePath, outPath);
+		else fs.copyFileSync(filePath, outPath);
+	});
+}
+
+// Run webpack command
+exec('yarn -v && yarn webpack', (stdErr, stdOut) => {
+	console.error(stdErr);
+	console.log(stdOut);
+
+	// Copy files from public folder
+	console.log('Copying public folder...');
+	copyFolder('public', 'build');
+
+	console.log('Build complete!');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,23 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const BUILD_PATH = path.resolve(__dirname, 'build');
+
 /** @type {webpack.Configuration} */
 module.exports = {
 	entry: path.resolve(__dirname, 'src', 'index.jsx'),
 	output: {
 		filename: '[name].[contenthash:8].js',
-		path: path.resolve(__dirname, 'build'),
+		publicPath: '/',
+		path: BUILD_PATH,
 	},
 	resolve: {
 		extensions: ['.js', '.jsx'],
+	},
+	devServer: {
+		hot: true,
+		port: 3000,
+		contentBase: BUILD_PATH,
 	},
 	module: {
 		rules: [
@@ -23,10 +31,15 @@ module.exports = {
 				test: /\.(s[ac]|c)ss$/,
 				use: ['style-loader', 'css-loader', 'sass-loader'],
 			},
+			{
+				test: /\.(png|jpe?g|webp|svg|gif)$/,
+				loader: 'file-loader',
+			},
 		],
 	},
 	plugins: [
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({ template: 'index.html' }),
+		new webpack.HotModuleReplacementPlugin(),
 	],
 };

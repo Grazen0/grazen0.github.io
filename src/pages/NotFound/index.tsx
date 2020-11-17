2abc239582a736036b0477536e0f841991127528
ElCholoGamer <josedanielgrayson@gmail.com>
2020-11-16 23:07:58 -0500

    Update index.tsx

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import { useHistory } from 'react-router-dom';
-import Fireplace from '../../assets/Fireplace.gif';
+import Fireplace from '../../assets/fireplace.gif';
 import './NotFound.scss';
 
 const NotFound: React.FC = () => {

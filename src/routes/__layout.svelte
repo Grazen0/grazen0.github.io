cf8f562a73033fff2560418a6ec3c7599e802ad2
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-23 01:42:29 -0500

    refactor: replace gtag script with GoogleAnalytics component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,12 @@
 <script>
+	import { GoogleAnalytics } from '@beyonk/svelte-google-analytics';
 	import '../global.css';
 </script>
 
 <svelte:head>
 	<title>ElCholoGamer.tk</title>
 </svelte:head>
+
+<GoogleAnalytics properties={[import.meta.env.VITE_GTAG_ID]} />
 
 <slot />

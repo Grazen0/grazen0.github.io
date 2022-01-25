cf3ae5196c51d1d78c44b6ce0e1264bcb7967e56
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-25 13:20:24 -0500

    refactor(background): remove initial objects and fix positioning

--- src/lib/components/TheStrongestBackground.svelte
+++ src/lib/components/TheStrongestBackground.svelte
@@ -6,20 +6,10 @@
 	let canvas: HTMLCanvasElement;
 
 	onMount(async () => {
-		// canvas.oncontextmenu = e => {
-		// 	e.preventDefault();
-		// 	e.stopPropagation();
-		// };
 		const gl = canvas.getContext('webgl');
 		if (!gl) return;
 
 		const scene = await BackgroundScene.init(gl, '/assets/images/cirno_prism.png');
-
-		for (let i = 0; i < 30; i++) {
-			const prism = scene.addPrism();
-			prism.position[1] = randomRange(-12, 16);
-		}
-
 		scene.start();
 
 		return () => scene.stop();
@@ -34,13 +24,12 @@
 
 <style>
 	canvas {
-		position: fixed;
 		width: 100%;
 		height: 100%;
-		top: 50%;
-		left: 50%;
-
-		transform: translate(calc(-50% - 0.5px), calc(-50% - 0.5px));
+		display: block;
+		position: fixed;
+		top: 0;
+		left: 0;
 		z-index: -9999;
 	}
 </style>

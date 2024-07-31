328af59da783df456c4c44c889a90fac017b931b
ElCholoGamer <josedanielgrayson@proton.me>
2024-07-30 23:52:02 -0500

    refactor: migrate to latest sveltekit version

--- src/global.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="@sveltejs/kit" />
-
-declare module '@beyonk/svelte-google-analytics';
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter(),
-		target: '#svelte',
+		adapter: adapter({
+			fallback: '404.html',
+		}),
 	},
 };
 

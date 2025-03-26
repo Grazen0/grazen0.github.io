b30586ad947980b728dcb37920f212d5f0523724
Grazen0 <josedanielgrayson@proton.me>
2025-03-26 18:10:10 -0500

    feat: add underline to header links

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -9,11 +9,11 @@
 </script>
 
 <header class="flex px-6 py-4">
-  <a href="{base}/" class="mr-4 sm:mr-10 text-xl font-bold">Grazen</a>
+  <a href="{base}/" class="hover:underline mr-4 sm:mr-10 text-xl font-bold">Grazen</a>
 
   <nav class="mx-4 flex grow items-center space-x-4 sm:space-x-10">
     {#each links as link (link.to)}
-      <a href={link.to} class="my-auto">{link.label}</a>
+      <a href={link.to} class="my-auto hover:underline">{link.label}</a>
     {/each}
   </nav>
 

50829a91e4483217994596af90c2e1c5926fef29
Grazen0 <josedanielgrayson@proton.me>
2025-03-26 19:43:20 -0500

    fix: better style menu accessibility

--- src/lib/components/layout/StyleMenu.svelte
+++ src/lib/components/layout/StyleMenu.svelte
@@ -3,7 +3,7 @@
   import { getActiveProseFont, getActiveThemeName } from '$lib/global-state/theme.svelte';
   import themes from '$lib/themes';
   import { onMount } from 'svelte';
-  import type { HTMLAttributes } from 'svelte/elements';
+  import type { HTMLAttributes, KeyboardEventHandler, MouseEventHandler } from 'svelte/elements';
   import { on } from 'svelte/events';
 
   const sortedThemeNames = Object.keys(themes).toSorted() as (keyof typeof themes)[];
@@ -21,8 +21,15 @@
     showMenu = !showMenu;
   };
 
-  const handleMenuClick = (ev: MouseEvent) => {
+  const handleMenuClick: MouseEventHandler<HTMLElement> = (ev) => {
     ev.stopPropagation();
+  };
+
+  const handleMenuKeyDown: KeyboardEventHandler<HTMLElement> = (ev) => {
+    if (ev.key === 'Escape' && showMenu) {
+      ev.preventDefault();
+      showMenu = false;
+    }
   };
 
   onMount(() =>
@@ -35,6 +42,8 @@
 <div class="relative">
   <button
     onclick={toggleMenu}
+    aria-haspopup="menu"
+    aria-expanded={showMenu}
     class={[
       'cursor-pointer border px-2 py-1 text-sm',
       showMenu ? 'text-fg border-fg' : 'text-fg-muted border-fg-muted',
@@ -44,47 +53,52 @@
   </button>
 
   {#if showMenu}
-    <form
+    <div
       onclick={handleMenuClick}
-      class="bg-bg border border-fg px-4 py-3 absolute top-4/3 right-0 space-y-4"
+      onkeydown={handleMenuKeyDown}
+      role="menu"
+      tabindex={-1}
+      class="bg-bg border border-fg px-4 py-4 absolute top-4/3 right-0"
     >
-      <div class="text-nowrap text-right">
-        <label for="theme" class="text-sm">Theme:</label>
-        <select
-          name="theme"
-          id="theme"
-          {...props}
-          class={['border-foreground border px-2 py-1 text-sm', className]}
-          bind:value={() => getActiveThemeName(), (v) => (globalState.theme = v)}
-        >
-          <optgroup label="dark">
-            {#each darkThemeNames as themeName (themeName)}
-              <option value={themeName}>{themeName}</option>
-            {/each}
-          </optgroup>
-          <optgroup label="light">
-            {#each lightThemeNames as themeName (themeName)}
-              <option value={themeName}>{themeName}</option>
-            {/each}
-          </optgroup>
-        </select>
-      </div>
+      <form class="space-y-4">
+        <div class="text-nowrap text-right">
+          <label for="theme" class="text-sm">Theme:</label>
+          <select
+            name="theme"
+            id="theme"
+            {...props}
+            class={['border-foreground border px-2 py-1 text-sm', className]}
+            bind:value={() => getActiveThemeName(), (v) => (globalState.theme = v)}
+          >
+            <optgroup label="dark">
+              {#each darkThemeNames as themeName (themeName)}
+                <option value={themeName}>{themeName}</option>
+              {/each}
+            </optgroup>
+            <optgroup label="light">
+              {#each lightThemeNames as themeName (themeName)}
+                <option value={themeName}>{themeName}</option>
+              {/each}
+            </optgroup>
+          </select>
+        </div>
 
-      <div class="text-nowrap text-right">
-        <label for="font-prose" class="text-sm">Prose font:</label>
-        <select
-          name="font-prose"
-          id="font-prose"
-          {...props}
-          class={['border-foreground border px-2 py-1 text-sm', className]}
-          bind:value={() => getActiveProseFont(), (v) => (globalState.proseFont = v)}
-        >
-          {#each FONT_TYPES as fontType (fontType)}
-            <option value={fontType}>{fontType}</option>
-          {/each}
-        </select>
-      </div>
-    </form>
+        <div class="text-nowrap text-right">
+          <label for="font-prose" class="text-sm">Prose font:</label>
+          <select
+            name="font-prose"
+            id="font-prose"
+            {...props}
+            class={['border-foreground border px-2 py-1 text-sm', className]}
+            bind:value={() => getActiveProseFont(), (v) => (globalState.proseFont = v)}
+          >
+            {#each FONT_TYPES as fontType (fontType)}
+              <option value={fontType}>{fontType}</option>
+            {/each}
+          </select>
+        </div>
+      </form>
+    </div>
   {/if}
 </div>
 <!-- Needed so that --font-serif is included in the generated CSS -->

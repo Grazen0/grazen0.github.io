8737c6787759b3cd994f952878643130b3f80676
Grazen0 <josedanielgrayson@proton.me>
2025-03-27 02:58:27 -0500

    feat: add feed.xml link and remove posts search bar

--- src/lib/search.ts
+++
@@ -1,8 +0,0 @@
-export type Token = string;
-export type Query = Token[];
-
-export const parseTokens = (text: string): Token[] => text.toLowerCase().trim().split(/\s+/);
-
-export const containsToken = (text: string) => (token: Token) => text.toLowerCase().includes(token);
-
-export const matchesQuery = (query: Query) => (text: string) => query.every(containsToken(text));
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import type { Post } from '$lib/blog';
+  import { base } from '$app/paths';
+  import Link from '$lib/components/Link.svelte';
   import Main from '$lib/components/layout/Main.svelte';
   import Title from '$lib/components/layout/Title.svelte';
-  import { matchesQuery, parseTokens, type Query } from '$lib/search';
   import type { PageProps } from './$types';
   import PostCard from './PostCard.svelte';
   import { onMount } from 'svelte';
@@ -10,17 +10,7 @@
 
   const { data }: PageProps = $props();
 
-  let queryText = $state('');
   let searchInputElement = $state<HTMLInputElement | null>(null);
-
-  const postMatchesQuery = (query: Query) => (post: Post) => {
-    const matcher = matchesQuery(query);
-    return matcher(post.title) || matcher(post.summary) || post.tags.some(matcher);
-  };
-
-  const query = $derived(parseTokens(queryText));
-
-  const filteredPosts = $derived(data.posts.filter(postMatchesQuery(query)));
 
   onMount(() =>
     on(document, 'keydown', (ev) => {
@@ -35,17 +25,12 @@
 <Main>
   <Title topic="Blog" />
 
-  <form class="text-center my-6">
-    <input
-      bind:value={queryText}
-      bind:this={searchInputElement}
-      placeholder="Search..."
-      class="border border-fg w-64 px-2 py-1 text-sm"
-    />
-  </form>
+  <div class="text-right">
+    <Link external href="{base}/blog/feed.xml">feed.xml</Link>
+  </div>
 
   <ul>
-    {#each filteredPosts as post (post.slug)}
+    {#each data.posts as post (post.slug)}
       <PostCard {post} />
     {/each}
   </ul>

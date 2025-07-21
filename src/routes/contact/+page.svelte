0b7be015ec43445b25d190053f4fcaeda7a0e61b
José Daniel Grayson <josedanielgrayson@proton.me>
2025-07-20 22:23:48 -0500

    feat: add icons to contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,21 +1,48 @@
 <script lang="ts">
   import Link from '$lib/components/Link.svelte';
-  import Stronger from '$lib/components/Stronger.svelte';
   import Main from '$lib/components/layout/Main.svelte';
   import Prose from '$lib/components/layout/Prose.svelte';
   import Title from '$lib/components/layout/Title.svelte';
+  import { faBluesky, faDiscord, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+  import Fa from 'svelte-fa';
+
+  const links = [
+    {
+      label: 'Discord',
+      to: 'https://discord.com/users/857769260604588052',
+      icon: faDiscord,
+    },
+    {
+      label: 'GitHub',
+      to: 'https://github.com/Grazen0',
+      icon: faGithub,
+    },
+    {
+      label: 'Twitter / X',
+      to: 'https://x.com/Grazen0',
+      icon: faTwitter,
+    },
+    {
+      label: 'BlueSky',
+      to: 'https://bsky.app/profile/grazen0.bsky.social',
+      icon: faBluesky,
+    },
+  ] as const;
 </script>
 
 <Main>
   <Title topic="Contact" />
   <Prose>
-    <p>
-      Feel free to send me a friend request on my Discord, <Stronger>grazen0</Stronger>. You can
-      also reach out to me with these links:
-    </p>
+    <p>Feel free to reach out to me for whatever. Here are some useful links:</p>
     <ul>
-      <li><Link external href="https://github.com/Grazen0">GitHub</Link></li>
-      <li><Link external href="https://x.com/Grazen0">Twitter</Link></li>
+      {#each links as link (link.to)}
+        <li>
+          <Link external href={link.to} class="inline-flex items-center gap-x-2">
+            <Fa icon={link.icon} />
+            {link.label}
+          </Link>
+        </li>
+      {/each}
     </ul>
   </Prose>
 </Main>

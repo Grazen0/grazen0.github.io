f7e735379b21df7e0c6a4b836d155f2881573647
José Daniel Grayson <josedanielgrayson@proton.me>
2025-08-02 03:18:00 -0500

    feat: improve 404 page

--- src/routes/[...path]/+page.svelte
+++ src/routes/[...path]/+page.svelte
@@ -2,9 +2,36 @@
   import Main from '$lib/components/layout/Main.svelte';
   import Prose from '$lib/components/layout/Prose.svelte';
   import Title from '$lib/components/layout/Title.svelte';
+  import { faArrowLeft, faHouse, faMessage } from '@fortawesome/free-solid-svg-icons';
+  import Fa from 'svelte-fa';
 </script>
 
 <Main>
   <Title topic="404 - Page Not Found" headTopic="Page Not Found" />
-  <Prose class="text-center">Maybe you misspelled something?</Prose>
+  <div class="text-center text-lg">
+    <Prose>Maybe you misspelled something?</Prose>
+  </div>
+  <div class="flex justify-center gap-4 text-sm sm:text-base">
+    <a
+      href="/"
+      class="text-fg-muted hover:text-fg border-fg-muted hover:border-fg inline-flex items-center justify-center gap-x-3 border px-3 py-2"
+    >
+      <Fa icon={faHouse} />
+      <span class="text-nowrap">Home</span>
+    </a>
+    <a
+      href="/blog"
+      class="text-fg-muted hover:text-fg border-fg-muted hover:border-fg inline-flex items-center justify-center gap-x-3 border px-3 py-2"
+    >
+      <Fa icon={faMessage} />
+      <span class="text-nowrap">Blog</span>
+    </a>
+    <button
+      onclick={() => window.history.back()}
+      class="text-fg-muted hover:text-fg border-fg-muted hover:border-fg inline-flex cursor-pointer items-center justify-center gap-x-3 border px-3 py-2"
+    >
+      <Fa icon={faArrowLeft} />
+      <span class="text-nowrap">Go back</span>
+    </button>
+  </div>
 </Main>

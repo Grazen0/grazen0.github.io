acbdac366b1ccb5415c989dd2e03d366e0e41b1d
José Daniel Grayson <josedanielgrayson@proton.me>
2025-08-07 00:31:28 -0500

    fix(blog): fix PostCard tags responsiveness

--- src/lib/blog/components/PostCard.svelte
+++ src/lib/blog/components/PostCard.svelte
@@ -17,20 +17,21 @@
 <li class="border-bg-light hover:border-fg border hover:no-underline">
   <a href={postUrl} class="block px-4 py-2">
     <h2 class="mb-2 text-xl font-semibold">{post.title}</h2>
-    <p class="mb-3">{post.summary}</p>
+    <p class="mb-2">{post.summary}</p>
 
-    <div class="inline-flex items-center gap-x-2 text-sm">
-      {#if post.createdAt}
-        <Fa icon={faCalendar} class="text-fg-muted" />
-        <span class="text-fg-muted">{dayjs.utc(post.createdAt).format('MMM D, YYYY')}</span>
-      {:else}
-        <Fa icon={faPencil} class="text-fg-muted" />
-        <span class="text-fg-muted">Draft</span>
-      {/if}
+    <div class="divide-fg-muted inline-flex gap-x-4 gap-y-3 text-sm not-sm:flex-col">
+      <div class="inline-flex items-center gap-x-2">
+        {#if post.createdAt}
+          <Fa icon={faCalendar} class="text-fg-muted" />
+          <span class="text-fg-muted">{dayjs.utc(post.createdAt).format('MMM D, YYYY')}</span>
+        {:else}
+          <Fa icon={faPencil} class="text-fg-muted" />
+          <span class="text-fg-muted">Draft</span>
+        {/if}
+      </div>
 
       {#if post.tags.length !== 0}
-        &middot;
-        <ul class="inline-flex items-center gap-x-2">
+        <ul class="inline-flex flex-wrap items-center gap-2">
           {#each post.tags as tag (tag)}
             <li class="bg-bg-light inline px-1.5 py-0.5 text-xs">#{tag}</li>
           {/each}
